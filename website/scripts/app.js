7da91ad708b97814c81ef6a4ad32f2146c2b7031
Laurens T. Rosinski <rosinski@duck.com>
2023-03-05 17:19:09 +0100

    added cursor

--- website/scripts/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-	const loader = document.querySelector(".loader");
-
-	loader.classList.add("loader--hidden");
-
-	loader.addEventListener("transitioned", () => {
-		document.body.removeChild(loader);
-	})
-})
